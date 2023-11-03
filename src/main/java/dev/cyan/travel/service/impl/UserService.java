da0f5ac9492e526c23bed3e9ec0beead36d488bc
Nazarii Ryhus <superkriper45@gmail.com>
2023-11-03 17:44:31 +0200

    userService implements IService

--- src/main/java/dev/cyan/travel/service/impl/UserService.java
+++ src/main/java/dev/cyan/travel/service/impl/UserService.java
@@ -1,8 +1,8 @@
-package dev.cyan.travel.service;
+package dev.cyan.travel.service.impl;
 
-import dev.cyan.travel.entity.Role;
 import dev.cyan.travel.entity.User;
 import dev.cyan.travel.repository.UserRepository;
+import dev.cyan.travel.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,27 +10,27 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements IService<User> {
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getUsers() {
+    @Override
+    public List<User> getAll() {
         return userRepository.findAll();
     }
 
-    public Optional<User> getUser(String id) {
+    @Override
+    public Optional<User> getById(String id) {
         return userRepository.findById(id);
     }
 
-    public User createUser(String username, String password, String firstName, String lastName, Role role) {
-        return userRepository.insert(new User(username, password, firstName, lastName, role));
-    }
-
-    public User saveUser(User user) {
+    @Override
+    public User createOrUpdate(User user) {
         return userRepository.save(user);
     }
 
-    public void deleteUser(String id) {
+    @Override
+    public void delete(String id) {
         userRepository.deleteById(id);
     }
 }
